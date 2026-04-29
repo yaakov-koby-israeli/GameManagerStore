1a4cd59cb7797677778c99dcef49ca9078007b99
koby <kobyisr1@gmail.com>
2026-04-29 14:53:19 +0300

    Export BASE_URL from api client for reuse

--- GameStore.React/src/shared/api/client.ts
+++ GameStore.React/src/shared/api/client.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:5090';
+export const BASE_URL = 'http://localhost:5090';
 
 async function request<T>(path: string, options?: RequestInit): Promise<T> {
   const response = await fetch(`${BASE_URL}${path}`, {
